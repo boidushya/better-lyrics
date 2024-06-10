7bd239aa30079f884cbad49bcf06e293629dacfc
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2024-06-10 09:31:58 +0530

    fix: favicon title description

--- www/@/components/landing.tsx
+++ www/@/components/landing.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { Button } from "./ui/button";
 import { Card } from "./ui/card";
 import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
 
--- www/pages/_document.tsx
+++ www/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="light">
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
