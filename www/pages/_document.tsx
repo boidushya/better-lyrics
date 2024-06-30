59c97b0c17b19e149d0c3b54b4a5f10c76fe313d
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2024-06-30 08:54:31 +0530

    fix(website): changed implementation location

--- www/pages/_document.tsx
+++ www/pages/_document.tsx
@@ -1,16 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en" className="light">
-      <Head>
-        <Script
-          defer
-          src="https://cloud.umami.is/script.js"
-          data-website-id="f9fdefc7-b1fe-49fd-973f-c312f9c824b6"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Landing } from "../components/landing";
+import Script from "next/script";
 
 export default function Home() {
   return (
@@ -17,6 +18,11 @@ export default function Home() {
           rel="preload"
           href="https://i.ibb.co/QFHpVfy/Screenshot-2024-06-04-at-22-33-35.png"
           as="image"
+        />
+        <Script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="f9fdefc7-b1fe-49fd-973f-c312f9c824b6"
         />
       </Head>
       <Landing />
