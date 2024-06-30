b4fe40e0ece00e1be2895818d836020eae41f570
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2024-06-30 08:47:20 +0530

    fix(website): updated structure

--- www/pages/_document.tsx
+++ www/pages/_document.tsx
@@ -1,9 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en" className="light">
-      <Head />
+      <Head>
+        <Script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="f9fdefc7-b1fe-49fd-973f-c312f9c824b6"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
