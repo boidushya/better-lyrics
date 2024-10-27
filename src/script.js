b1ef6ea943af45e1401c01d78e7b1d7ee398bf78
Varun Ahuja <varun7654@outlook.com>
2024-10-27 15:14:20 -0400

    clear/reregister the interval when the page loads/unloads
    Co-authored-by: Boidushya <boidushyabhattacharya@gmail.com>

--- src/script.js
+++ src/script.js
@@ -14,11 +14,40 @@ document.addEventListener("blyrics-get-song-info", function () {
   document.dispatchEvent(event);
 });
 
-setInterval(function() {
-  const player = document.getElementById("movie_player");
-  if (player) {
-    const currentTime = player.getCurrentTime();
-    const event = new CustomEvent("blyrics-send-player-time", { detail: { currentTime: currentTime } });
-    document.dispatchEvent(event);
+
+// Initialize the time update interval and stop it when the page is unloaded
+
+let timeUpdateInterval;
+
+const startTimeUpdate = () => {
+  stopTimeUpdate();
+
+  timeUpdateInterval = setInterval(function () {
+    const player = document.getElementById("movie_player");
+    if (player) {
+      try {
+        const currentTime = player.getCurrentTime();
+        document.dispatchEvent(
+          new CustomEvent("blyrics-send-player-time", {
+            detail: { currentTime: currentTime },
+          })
+        );
+      } catch (_) {
+        stopTimeUpdate();
+      }
+    } else {
+      stopTimeUpdate();
+    }
+  }, 20);
+}
+
+const stopTimeUpdate = () => {
+  if (timeUpdateInterval) {
+    clearInterval(timeUpdateInterval);
+    timeUpdateInterval = null;
   }
-}, 20);
+}
+
+window.addEventListener("unload", stopTimeUpdate);
+
+startTimeUpdate();
