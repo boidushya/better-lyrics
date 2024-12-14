48e7e633626389e6882334407f4ead452ca392dd
varun7654 <varun7654@outlook.com>
2024-12-14 15:13:28 -0500

    if yt lyrics are available, check that the lyrics we fetch match to them

--- src/modules/lyrics/lyrics.js
+++ src/modules/lyrics/lyrics.js
@@ -99,8 +99,18 @@ BetterLyrics.Lyrics = {
           if (!ytLyrics) {
             ytLyrics = await BetterLyrics.LyricProviders.ytLyrics(waitForLoaderPromise);
           }
+          if (ytLyrics.text !== BetterLyrics.Constants.NO_LYRICS_TEXT) {
+            let lyricText = "";
+            lyrics.lyrics.forEach((lyric) => {
+              lyricText += lyric.words + "\n";
+            })
 
-          // TODO compare ytLyrics w/ the returned lyrics and reject if they don't match.
+            let matchAmount = stringSimilarity(lyricText.toLowerCase(), ytLyrics.text.toLowerCase());
+            if (matchAmount < 0.8) {
+              BetterLyrics.Utils.log(`Got lyrics from ${lyrics.source}, but they don't match yt lyrics. Rejecting: Match: ${matchAmount}%`)
+              continue;
+            }
+          }
           break;
         }
       } catch (err) {
@@ -310,4 +320,40 @@ BetterLyrics.Lyrics = {
   setupLyricsCheckInterval: function () {
     BetterLyrics.App.areLyricsTicking = true;
   },
+};
+
+/**
+ * @author Stephen Brown
+ * Source: https://github.com/stephenjjbrown/string-similarity-js/
+ * @licence MIT License - https://github.com/stephenjjbrown/string-similarity-js/blob/master/LICENSE.md
+ * @param {string} str1 First string to match
+ * @param {string} str2 Second string to match
+ * @param {number} [substringLength=2] Optional. Length of substring to be used in calculating similarity. Default 2.
+ * @param {boolean} [caseSensitive=false] Optional. Whether you want to consider case in string matching. Default false;
+ * @returns Number between 0 and 1, with 0 being a low match score.
+ */
+var stringSimilarity = function (str1, str2, substringLength, caseSensitive) {
+  if (substringLength === void 0) { substringLength = 2; }
+  if (caseSensitive === void 0) { caseSensitive = false; }
+  if (!caseSensitive) {
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+  }
+  if (str1.length < substringLength || str2.length < substringLength)
+    return 0;
+  var map = new Map();
+  for (var i = 0; i < str1.length - (substringLength - 1); i++) {
+    var substr1 = str1.substr(i, substringLength);
+    map.set(substr1, map.has(substr1) ? map.get(substr1) + 1 : 1);
+  }
+  var match = 0;
+  for (var j = 0; j < str2.length - (substringLength - 1); j++) {
+    var substr2 = str2.substr(j, substringLength);
+    var count = map.has(substr2) ? map.get(substr2) : 0;
+    if (count > 0) {
+      map.set(substr2, count - 1);
+      match++;
+    }
+  }
+  return (match * 2) / (str1.length + str2.length - ((substringLength - 1) * 2));
 };
